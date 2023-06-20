a618baabc8b0e32cc3ac8aaba53cb0ea1363f210
Analyze_ <101422486+Analyze0@users.noreply.github.com>
2023-06-20 15:30:52 -0400

    Update useKeyboard.js

--- src/hooks/useKeyboard.js
+++ src/hooks/useKeyboard.js
@@ -12,6 +12,7 @@ function actionByKey(key) {
 		Digit3: 'glass',
 		Digit4: 'wood',
 		Digit5: 'log',
+		Shift: 'shiftPressed',
 	}
 	return keyActionMap[key]
 }
@@ -28,6 +29,7 @@ export const useKeyboard = () => {
 		glass: false,
 		wood: false,
 		log: false,
+		shiftPressed: false,
 	})
 
 	const handleKeyDown = useCallback((e) => {
